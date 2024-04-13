e5ffd5f1f4ffc48959e01e3c615757911f4c8c8f
MoAshrafPT <121402445+MoAshrafPT@users.noreply.github.com>
2024-04-13 17:49:49 +0200

    Added ALU

--- code/ALU.vhd
+++ code/ALU.vhd
@@ -15,13 +15,20 @@ architecture  of ALU is
     begin
         with ALUControl select
             Result <=
+            --ADD
                 A + B when "000",
+            --SUB    
                 A - B when "001",
+            --AND
                 A and B when "010",
+            --OR    
                 A or B when "011",
+            --XOR    
                 A xor B when "100",
+            --NOT A    
                 not A when "101",
-                not B when "110",
+            --CMP     
+                A - B when "110",
                 (others => '0') when others;
         Zero <= '1' when Result = (others => '0') else '0';
 
